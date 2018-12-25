09f7b68515c309fa16c0fb972e81c34e8f689dfd
Piorosen <aoikazto@gmail.com>
2018-12-25 23:15:11 +0900

    open GL 추가

--- OpenGL/OpenGL/소스.cpp
+++ OpenGL/OpenGL/소스.cpp
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <glut.h>
+
+
+void Display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBegin(GL_POLYGON);
+	glVertex3f(-0.5, -0.5, 0.0);
+	glVertex3f(0.5, -0.5, 0.0);
+	glVertex3f(0.5, 0.5, 0.0);
+	glVertex3f(-0.5, 0.5, 0.0);
+	glEnd();
+
+	glFlush();
+}
 
 int main() {
-	printf("Hello World");
-
+	glutCreateWindow("HelloOpenGL");
+	glutDisplayFunc(Display);
+	glutMainLoop();
 	return 0;
 }
