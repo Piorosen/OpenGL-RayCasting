b3d521ccd34f0abbbfece8f10723846571052f1b
Piorosen <aoikazto@gmail.com>
2018-12-27 14:52:32 +0900

    ll

--- OpenGL/소스.cpp
+++ OpenGL/소스.cpp
@@ -1,104 +1,75 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
 #include <glut.h>
-#include <math.h>
-#include <time.h>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
 
-#define PI 3.14159265
+#define PI 3.1415926535897
 
-float srand(time(NULL));
-float a = (rand() % 50 - 25.0) / (float)100.0;
-float b = (rand() % 50 - 25.0) / (float)100.0;
-float c = (rand() % 50) / (float)100.0;
+class Circle {
+private:
+	float x, y;
+	float size;
+	float R, G, B;
 
-double getRadian(int num)
-{
-	return num * (PI / 180.0);
-}
+	float getRadian(int num)
+	{
+		return num * (PI / 180.0);
+	}
+
+public:
+	void Init() {
+		x = rand() % 200 / 100.0f - 1.0f;
+		y = rand() % 200 / 100.0f - 1.0f;
+		R = rand() % 100 / 100.0f;
+		G = rand() % 100 / 100.0f;
+		B = rand() % 100 / 100.0f;
+
+		size = rand() % 100 / 100.0f;
+	}
+
+	void Draw() {
+
+		glBegin(GL_TRIANGLE_FAN);
+		glColor3f(R, G, B);
+		glVertex2f(x * size, y * size);
+		for (int i = 0; i < 361; i++)
+		{
+			double j = getRadian(i);
+			glVertex2f((cos(j) + x) * size, (sin(j) + y) * size);
+		}
+		glEnd();
+	}
+};
+
 
 void Display() {
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	glBegin(GL_TRIANGLE_FAN);
-
-	glVertex2f(a, b);
-
-	for (int i = 0; i < 361; i++)
-	{
-		float j = getRadian(i);
-		float x = c * cos(j), y = c * sin(j);
-		glVertex2f(x + a, y + b);
+	glClear(GL_COLOR_BUFFER_BIT);//버퍼를 싹다 정리하고 새롭게 그린다
+	Circle c[10];
+	for (int i = 0; i < 10; i++) {
+		c[i].Init();
+		c[i].Draw();
 	}
-
-	glEnd();
-
 	glFlush();
 }
 
-void Display3() {
-	glClear(GL_COLOR_BUFFER_BIT);
+void DrawCircle() {
 
-	glBegin(GL_TRIANGLE_FAN);
-
-	glVertex2f(a, b);
-
-	for (int i = 0; i < 361; i++)
-	{
-		float j = getRadian(i);
-		float x = c * cos(j), y = c * sin(j);
-		glVertex2f(x + a, y + b);
-	}
-
-	glVertex2f(a, -b);
-
-	for (int i = 0; i < 361; i++)
-	{
-		float j = getRadian(i);
-		float x = c * cos(j), y = c * sin(j);
-		glVertex2f(x + a, y - b);
-	}
-
-	glVertex2f(-a, b);
-
-	for (int i = 0; i < 361; i++)
-	{
-		float j = getRadian(i);
-		float x = c * cos(j), y = c * sin(j);
-		glVertex2f(x - a, y + b);
-	}
-
-	glEnd();
-
-	glFlush();
 }
+void Keyboard(unsigned char ch, int x1, int y1) {
+	if (ch == 'q') {
+		exit(0);
+	}
+	else if (ch == 'e') {
+		glutPostRedisplay();
+	}
 
-void Keyboard(unsigned char ch, int x, int y)
-{
-	if (ch == 'Q' || ch == 'q')
-	{
-		exit(1);
-	}
-	else if (ch == 'E' || ch == 'e') {
-		glutDisplayFunc(Display3);
-	}
-	else if (ch == 'W' || ch == 'w') {
-		b += 0.01f;
-	}
-	else if (ch == 'S' || ch == 's') {
-		b -= 0.01f;
-	}
-	else if (ch == 'A' || ch == 'a') {
-		a -= 0.01f;
-	}
-	else if (ch == 'D' || ch == 'd') {
-		a += 0.01f;
-	}
-	glutPostRedisplay();
 }
 
 int main() {
-	glutCreateWindow("HelloOpenGL");
-	glutDisplayFunc(Display);
+	srand((unsigned)time(0));
+	glutCreateWindow("HelloOpenGL");//그림을 표현하는 콘솔
+	glutDisplayFunc(Display);//그림 그리는 함수
 	glutKeyboardFunc(Keyboard);
 	glutMainLoop();
 	return 0;
